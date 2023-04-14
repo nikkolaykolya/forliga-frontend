1fe42b03a5921e0bc885d1ea681f7bcbf093f3c4
Andrei <86118324+AnDrE1Leb@users.noreply.github.com>
2023-04-14 04:27:19 +0300

    2 popups and start of third

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-let gamesSearch = document.getElementById("gamesSearch");
-let gamesSearchDrop = document.getElementById("gamesSearchDrop");
+const gamesSearch = document.getElementById("gamesSearch");
+const gamesSearchDrop = document.getElementById("gamesSearchDrop");
 
 gamesSearch.addEventListener('focus', function (){
     if (document.activeElement === gamesSearch) {
@@ -10,3 +10,39 @@ gamesSearch.addEventListener('focus', function (){
 gamesSearch.addEventListener('blur', function (){
     gamesSearchDrop.classList.remove("show");
 });
+
+
+const chatForm = document.getElementById("chatForm");
+const contacts = document.getElementById("contacts");
+const chatExit = document.getElementById("chatExit");
+const chatSubmit = document.getElementById("chatSubmit");
+
+contacts.addEventListener('click', function () {
+    chatForm.classList.add("show");
+    document.body.style.overflow = 'hidden';
+})
+
+chatExit.addEventListener('click', function () {
+    chatForm.classList.remove("show");
+    document.body.style.overflow = 'unset';
+})
+
+chatSubmit.addEventListener('click', function (){
+    chatForm.classList.remove("show");
+    document.body.style.overflow = 'unset';
+})
+
+const startPopup = document.getElementById("startPopup");
+const startBtn = document.getElementById("startBtn");
+const startExit = document.getElementById("startExit");
+
+
+startBtn.addEventListener('click', function () {
+    startPopup.classList.add("show");
+    document.body.style.overflow = 'hidden';
+})
+
+startExit.addEventListener('click', function () {
+    startPopup.classList.remove("show");
+    document.body.style.overflow = 'unset';
+})
