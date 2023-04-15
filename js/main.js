2460728804751dace73b8f5ee7542b8d9b2166af
Andrei <86118324+AnDrE1Leb@users.noreply.github.com>
2023-04-15 03:22:47 +0300

    Finished popups

--- js/main.js
+++ js/main.js
@@ -46,3 +46,75 @@ startExit.addEventListener('click', function () {
     startPopup.classList.remove("show");
     document.body.style.overflow = 'unset';
 })
+
+const enterPopup = document.getElementById("enterPopup");
+const enter = document.getElementById("enter");
+const enterExit = document.getElementById("enterExit");
+
+
+enter.addEventListener('click', function () {
+    enterPopup.classList.add("show");
+    document.body.style.overflow = 'hidden';
+})
+
+enterExit.addEventListener('click', function () {
+    enterPopup.classList.remove("show");
+    document.body.style.overflow = 'unset';
+})
+
+const mailFormEnterBtn = document.getElementById("mailFormEnterBtn");
+const smsFormEnterBtn = document.getElementById("smsFormEnterBtn");
+const mailEnterForm = document.getElementById("mailEnterForm");
+const smsEnterForm = document.getElementById("smsEnterForm");
+
+
+mailFormEnterBtn.addEventListener('click', function (){
+    mailEnterForm.classList.add('active');
+    smsEnterForm.classList.remove('active');
+    mailFormEnterBtn.classList.add('active');
+    smsFormEnterBtn.classList.remove('active');
+})
+
+smsFormEnterBtn.addEventListener('click', function (){
+    smsEnterForm.classList.add('active');
+    mailEnterForm.classList.remove('active');
+    smsFormEnterBtn.classList.add('active');
+    mailFormEnterBtn.classList.remove('active');
+})
+
+
+const registrationPopup = document.getElementById("registrationPopup");
+const registration = document.getElementById("registration");
+const registrationExit = document.getElementById("registrationExit");
+
+
+registration.addEventListener('click', function () {
+    registrationPopup.classList.add("show");
+    document.body.style.overflow = 'hidden';
+})
+
+registrationExit.addEventListener('click', function () {
+    registrationPopup.classList.remove("show");
+    document.body.style.overflow = 'unset';
+})
+
+
+const mailFormRegistrationBtn = document.getElementById("mailFormRegistrationBtn");
+const smsFormRegistrationBtn = document.getElementById("smsFormRegistrationBtn");
+const mailRegistrationForm = document.getElementById("mailRegistrationForm");
+const smsRegistrationForm = document.getElementById("smsRegistrationForm");
+
+
+mailFormRegistrationBtn.addEventListener('click', function (){
+    mailRegistrationForm.classList.add('active');
+    smsRegistrationForm.classList.remove('active');
+    mailFormRegistrationBtn.classList.add('active');
+    smsFormRegistrationBtn.classList.remove('active');
+})
+
+smsFormRegistrationBtn.addEventListener('click', function (){
+    smsRegistrationForm.classList.add('active');
+    mailRegistrationForm.classList.remove('active');
+    smsFormRegistrationBtn.classList.add('active');
+    mailFormRegistrationBtn.classList.remove('active');
+})
